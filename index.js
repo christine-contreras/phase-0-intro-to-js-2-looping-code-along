54c6581ee0f2f7065b3477865c5970784fc7ae91
Christine Contreras <christineec23@yahoo.com>
2021-03-15 11:05:00 -0500

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++ ) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    // debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts); 
+
+
+
+//FOR LOOP
+const names = ['Ada', 'Brendan', 'Ali'];
+
+function writeCards(names, event) {
+    const newArray = [];
+    for (let i = 0; i < names.length; i++ ) {
+        let string = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        newArray.push(string);
+    }
+
+    console.log(newArray);
+    return newArray;
+}
+
+writeCards(names, 'birthday');
+
+
+//WHILE LOOP
+function countDown(num) {
+    let i = num;
+    while (i >= 0 ){
+        console.log(i--);
+    }
+}
+
+countDown(10);
